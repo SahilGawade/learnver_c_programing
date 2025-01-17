2e7f712231ca9aa1743bed45060fc6152fa7c784
SahilGawade <sahilgawade828@gmail.com>
2025-01-17 11:10:37 +0530

    Create tem.c

---
+++ tem.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main (){
+
+
+char src[100]="sarkar group ";
+char  dest[100];
+strcpy(dest,src);
+printf("dest=%s",dest);
+
+printf("len of str=%d\n",strlen("hello ajay"));
+
+char abc[10]="sahil";
+char temp[10];
+   
+
+   
+
+
+
+    return 0;
+}
